5de1312952e45131d7431cb58774b75f32ef5be6
pjsvis <peter@visibility.net>
2015-11-02 18:57:41 +0000

    Changed gulp task to stop watching and let VS Code do the watching as gulp stops on error even with stream-combiner2. Possibly misconfigured.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var combiner = require('stream-combiner2');
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('less', function() {
+gulp.task('default', function() {
     var combined = combiner.obj([
         gulp.src('custom/custom-bootstrap.less')
             .pipe(less())
@@ -15,6 +15,7 @@ gulp.task('less', function() {
     return combined;
 });
 
-gulp.task('default', function() {
+// Let VS Code do the watching as gulp exits on error
+gulp.task('less-watch', function() {
     gulp.watch('custom/*.less', ['less']);
 })
