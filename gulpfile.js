b9f9636c1f851611fa06aa5abe23c556195f8b50
pjsvis <peter@visibility.net>
2015-11-03 19:42:14 +0000

    Added gulp-plumber and gulp-utils

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,23 @@ var combiner = require('stream-combiner2');
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('default', function() {
-    var combined = combiner.obj([
+gulp.task('less', function() {
         gulp.src('custom/custom-bootstrap.less')
+        .pipe(plumber({
+           handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+        }))
             .pipe(less())
             .pipe(gulp.dest(function (f) {
                 return f.base;
             }))        
-    ]);
-    combined.on('error', console.error.bind(console));
-    return combined;
 });
 
 // Let VS Code do the watching as gulp exits on error
-gulp.task('less-watch', function() {
-    gulp.watch('custom/*.less', ['less']);
+gulp.task('default', function() {
+     gulp.watch('custom/*.less', ['less']);
+     return gulp.on('error', console.error.bind(console));
+    // return gulp;
 })
