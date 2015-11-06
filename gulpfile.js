b81b046e07ed2eb55525a42c4734dee27e642799
pjsvis <peter@visibility.net>
2015-11-06 18:49:55 +0000

    Changed the gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,33 @@
 // Less configuration
-var plumber = require('gulp-plumber');
+var combiner = require('stream-combiner2');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var less = require('gulp-less');
 
-gulp.task('less', function() {
-        gulp.src('custom/custom-bootstrap.less')
-        .pipe(plumber({
-           handleError: function (err) {
-            console.log(err);
-            this.emit('end');
-        }
-        }))
-            .pipe(less())
-            .pipe(gulp.dest(function (f) {
-                return f.base;
-            }))        
+gulp.task('default', function(){
+    gulp.src('custom/custom-bootstrap.less');
+    less();
 });
 
+gulp.task('combined', function() {
+  var combined = combiner.obj([
+    gulp.src('custom/custom-bootstrap.less'),
+    less(),
+    gulp.dest('custom')
+  ]);
+
+  // any errors in the above streams will get caught
+  // by this listener, instead of being thrown:
+  combined.on('error', console.error.bind(console));
+  return combined;
+});
+
+
+
 // Let VS Code do the watching as gulp exits on error
-gulp.task('default', function() {
-     gulp.watch('custom/*.less', ['less']);
+gulp.task('less-combined', function() {
+     gulp.watch('custom/*.less', ['combined']);
      return gulp.on('error', console.error.bind(console));
-    // return gulp;
+     return gulp;
 })
+
