cf965b78d7d3aab0a44964ad0f1e93f29022ccb9
pjsvis <peter@visibility.net>
2015-11-07 13:18:30 +0000

    Installed bower and angular and ui-bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,15 @@
 // Less configuration
-var combiner = require('stream-combiner2');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function(){
-    gulp.src('custom/custom-bootstrap.less')
+    return gulp.src('custom/custom-bootstrap.less')
+    .pipe(sourcemaps.init())    
     .pipe(less())
+    .pipe(sourcemaps.write('custom'))
     .pipe(gulp.dest('custom'));
 });
 
-gulp.task('combined', function() {
-  var combined = combiner.obj([
-    gulp.src('custom/custom-bootstrap.less'),
-    less(),
-    gulp.dest('custom')
-  ]);
 
-  // any errors in the above streams will get caught
-  // by this listener, instead of being thrown:
-  combined.on('error', console.error.bind(console));
-  return combined;
-});
-
-
-
-// Let VS Code do the watching as gulp exits on error
-gulp.task('less-combined', function() {
-     gulp.watch('custom/*.less', ['combined']);
-     return gulp.on('error', console.error.bind(console));
-     return gulp;
-})
 
